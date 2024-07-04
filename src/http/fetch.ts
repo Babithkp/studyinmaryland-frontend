b84b50aaf09df42233d8b4fb5bdffc5d44ab02ad
Babithkp <babithkullachetti44@gmail.com>
2024-07-04 15:46:12 +0530

    vercel url changed

--- src/http/fetch.ts
+++ src/http/fetch.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-// const vercelUrl = "https://studyinmaryland-backend.vercel.app/api/fileupload";
-const localUrl = "http://localhost:3000/api/fileupload"
+const vercelUrl = "https://studyinmaryland-backend.vercel.app/api/fileupload";
+// const localUrl = "http://localhost:3000/api/fileupload"
 
 export const uploadsingleFile = async (file: unknown) => {
   
-  const response = await axios.post(`${localUrl}`, file, {
+  const response = await axios.post(`${vercelUrl}/fileupload`, file, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -13,6 +13,6 @@ export const uploadsingleFile = async (file: unknown) => {
 };
 export const newUserregistration = async (file: unknown) => {
   
-  const response = await axios.post(`http://localhost:3000/api/newRegistration`, file);
+  const response = await axios.post(`${vercelUrl}/newRegistration`, file);
   console.log(response);
 };
